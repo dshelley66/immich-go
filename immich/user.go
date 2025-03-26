550f8a7528cb06b9da75a06245b21c78c05f1fb9
Doug Shelley <dougshelley66@gmaiL.com>
2025-03-26 19:36:13 -0400

    WIP new album commands with regex

--- immich/user.go
+++ immich/user.go
@@ -5,15 +5,19 @@ import "time"
 type User struct {
 	ID                   string    `json:"id"`
 	Email                string    `json:"email"`
-	FirstName            string    `json:"firstName"`
-	LastName             string    `json:"lastName"`
-	StorageLabel         string    `json:"storageLabel"`
-	ExternalPath         string    `json:"externalPath"`
+	Name                 string    `json:"name"`
 	ProfileImagePath     string    `json:"profileImagePath"`
+	AvatarColor          string    `json:"avatarColor"`
+	ProfileChangedAt     time.Time `json:"profileChangedAt"`
+	StorageLabel         string    `json:"storageLabel"`
 	ShouldChangePassword bool      `json:"shouldChangePassword"`
 	IsAdmin              bool      `json:"isAdmin"`
 	CreatedAt            time.Time `json:"createdAt"`
 	DeletedAt            time.Time `json:"deletedAt"`
 	UpdatedAt            time.Time `json:"updatedAt"`
 	OauthID              string    `json:"oauthId"`
+	QuotaSizeInBytes     int64     `json:"quotaSizeInBytes"`
+	QuotaUsageInBytes    int64     `json:"quotaUsageInBytes"`
+	Status               string    `json:"status"`
+	License              string    `json:"license"`
 }
---
+++ internal/formats/list_format.go
@@ -0,0 +1,21 @@
+package formats
+
+import (
+	"github.com/olekukonko/tablewriter"
+	"io"
+)
+
+// OutFormatForList This configures the table format for the List commands
+func OutFormatForList(out io.Writer) *tablewriter.Table {
+	table := tablewriter.NewWriter(out)
+	table.SetAutoWrapText(false)
+	table.SetAutoFormatHeaders(true)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetCenterSeparator("")
+	table.SetColumnSeparator("")
+	table.SetRowSeparator("")
+	table.SetHeaderLine(false)
+	table.SetBorder(false)
+	return table
+}
